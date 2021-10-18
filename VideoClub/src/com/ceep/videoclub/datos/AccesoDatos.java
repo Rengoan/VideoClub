7bfc7991e43099606201cd0d59cfb5e3d561ea2c
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-10-18 12:44:56 +0200

    2

---
+++ VideoClub/src/com/ceep/videoclub/datos/AccesoDatos.java
@@ -0,0 +1,32 @@
+
+package com.ceep.videoclub.datos;
+
+import com.ceep.videoclub.domain.Pelicula;
+
+
+public class AccesoDatos {
+    
+    public boolean existe(String nombreArchivo){
+        return true;
+    }
+    
+    public void listar(String nombreArchivo){
+        
+    }
+    
+    public void escribir(Pelicula pelicula, String nombreArchivo, boolean anexar){
+        
+    }
+    
+    public void buscar(String nombreArchivo, String buscar){
+        
+    }
+    
+    public void crear (String nombreArchivo){
+        
+    }
+    
+    public void borrar (String nombreArchivo){
+        
+    }
+}
---
+++ VideoClub/src/com/ceep/videoclub/domain/Pelicula.java
@@ -0,0 +1,34 @@
+
+package com.ceep.videoclub.domain;
+
+public class Pelicula {
+    private String nombre;
+    
+    //Constructor
+
+    public Pelicula() {
+    }
+
+    public Pelicula(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //Getter and Setter
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //toString
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "nombre=" + nombre + '}';
+    }
+    
+    
+}
